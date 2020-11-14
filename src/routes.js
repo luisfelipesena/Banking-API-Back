c0f9f0f466a1d4bbe6a72893d4e79562fb3ca56f
Luis Felipe <luisfelipesena@gmail.com>
2020-11-14 17:29:43 -0300

    feat: added login, createUser, createClient, getByEmail routes

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,15 @@ const Router = require('koa-router');
 
 const router = new Router();
 
+const authenticationController = require('./controllers/auth');
+
+const usersController = require('./controllers/users');
+const clientsController = require('./controllers/clients');
+
+const sessionMiddleware = require('./middlewares/session');
+
+router.post('/auth', authenticationController);
+router.post('/usuarios', usersController.createUser);
+router.post('/clientes', sessionMiddleware, clientsController.createClient);
+
 module.exports = router;
