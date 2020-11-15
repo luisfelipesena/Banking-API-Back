936a70646024c498f46da20300f6003a2adcbca2
Luis Felipe <luisfelipesena@gmail.com>
2020-11-15 16:07:18 -0300

    feat: password encrypt added

---
+++ src/middlewares/encrypt.js
@@ -0,0 +1,15 @@
+const response = require('../utils/response');
+const Password = require('../utils/password');
+
+const encrypt = async (ctx, next) => {
+	const { senha = null } = ctx.request.body;
+	if (!senha) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado' });
+	}
+
+	const hash = await Password.encrypt(senha);
+	ctx.state.hash = hash;
+	return next();
+};
+
+module.exports = encrypt;
