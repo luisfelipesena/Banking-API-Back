c87994493c9f9d7f12abed21c21fe3bf27d92fe7
Luis Felipe <luisfelipesena@gmail.com>
2020-11-15 16:01:35 -0300

    feat: password encrypt added

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,11 @@ const usersController = require('./controllers/users');
 const clientsController = require('./controllers/clients');
 
 const sessionMiddleware = require('./middlewares/session');
+const encryptMiddleware = require('./middlewares/encrypt');
 
 router.post('/auth', authenticationController);
-router.post('/usuarios', usersController.createUser);
+router.post('/usuarios', encryptMiddleware, usersController.createUser);
 router.post('/clientes', sessionMiddleware, clientsController.createClient);
+router.put('/clientes', sessionMiddleware, clientsController.editClient);
 
 module.exports = router;
