8f70386cd18b7146d9b58eafde9b7c1bbacfd447
Luis Felipe <luisfelipesena@gmail.com>
2020-11-17 22:07:12 -0300

    feat: calculateCharges,createChargem listCharge and payCharge

---
+++ src/controllers/charges.js
@@ -0,0 +1,131 @@
+const ChargesRepository = require('../repositories/charges');
+const ClientsRepository = require('../repositories/clients');
+const PagarMe = require('./pagarme');
+const response = require('../utils/response');
+
+const calculateCharges = async (id) => {
+	const charges = await ChargesRepository.getChargesById(id);
+	let [cobrancasFeitas, cobrancasRecebidas, estaInadimplente] = [0, 0, false];
+
+	if (charges) {
+		charges.forEach((charge) => {
+			cobrancasFeitas += Number(charge.valor);
+			if (charge.status === 'pago') {
+				cobrancasRecebidas += Number(charge.valor);
+			}
+		});
+
+		if (cobrancasFeitas > cobrancasRecebidas) {
+			estaInadimplente = true;
+		}
+	}
+	return { cobrancasFeitas, cobrancasRecebidas, estaInadimplente };
+};
+/**
+ * Cria uma nova Cobrança e previne erros com o valor e a data
+ */
+const createCharge = async (ctx) => {
+	const {
+		idDoCliente = null,
+		descricao = null,
+		valor = null,
+		vencimento = null,
+	} = ctx.request.body;
+
+	const [ano, mes, dia] = vencimento.split('/').reverse();
+	if (!idDoCliente || !descricao || !valor || !vencimento) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado' });
+	} else if (
+		!new Date(ano, mes, dia) ||
+		vencimento.length !== 10 ||
+		+new Date(ano, mes - 1, dia) < +new Date()
+	) {
+		return response(ctx, 400, {
+			mensagem: 'Data de vencimento mal formatada',
+		});
+	} else if (Number(valor) <= 0) {
+		return response(ctx, 400, { mensagem: 'Valor mal formatado' });
+	}
+
+	const { nome, email, cpf, tel } = await ClientsRepository.getClientById(
+		idDoCliente
+	);
+
+	const transaction = await PagarMe.gerarBoleto({
+		amount: Number(valor),
+		nome,
+		email,
+		cpf,
+		tel,
+	});
+
+	if (transaction) {
+		const createCharge = await ChargesRepository.createCharge({
+			idDoCliente,
+			descricao,
+			valor: Number(valor),
+			vencimento: vencimento.split('/').reverse().join('-'),
+		});
+
+		if (createCharge) {
+			const result = await ChargesRepository.inserirLinkBoleto(
+				transaction.boleto_url, // Caso postback_url -> .postback_url
+				createCharge.id
+			);
+			return response(ctx, 201, {
+				cobranca: { ...result, vencimento: vencimento.slice(0, 10) },
+			});
+		}
+	}
+	return response(ctx, 400, { mensagem: 'Cobrança mal formatada' });
+};
+
+const listCharges = async (ctx) => {
+	const { cobrancasPorPagina = null, offset = null } = ctx.query;
+	const { id } = ctx.state;
+	if (
+		!cobrancasPorPagina ||
+		!offset ||
+		isNaN(Number(cobrancasPorPagina)) ||
+		isNaN(Number(offset))
+	) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado' });
+	}
+
+	let charges = await ChargesRepository.getChargesByIdAndQuerys(
+		id,
+		offset,
+		cobrancasPorPagina
+	);
+
+	if (charges) {
+		return response(ctx, 200, {
+			cobrancas: charges.map((c) => {
+				let status = c.status;
+				if (+c.vencimento < +new Date()) {
+					status = 'vencido';
+				}
+				return {
+					id_do_cliente: c.id_do_cliente,
+					descricao: c.descricao,
+					valor: c.valor,
+					vencimento: c.vencimento.toLocaleDateString(),
+					link_do_boleto: c.link_do_boleto,
+					status,
+				};
+			}),
+		});
+	}
+	return response(ctx, 404, { mensagem: 'Cobranças não encontradas' });
+};
+
+const payCharge = async (ctx) => {
+	const { idDaCobranca = null } = ctx.request.body;
+	const payment = await ChargesRepository.payCharge(idDaCobranca);
+	if (payment) {
+		return response(ctx, 200, { status: payment.status });
+	}
+	return response(ctx, 404, { mensagem: 'Cobranças não encontradas' });
+};
+
+module.exports = { calculateCharges, createCharge, listCharges, payCharge };
