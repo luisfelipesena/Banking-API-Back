fc1c3302c851f88310968b311de44417f2788810
Luis Felipe <luisfelipesena@gmail.com>
2020-11-24 21:15:09 -0300

    refactor: index, charges, clients, emails

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const { Client } = require('pg');
-require('dotenv').config();
-
-const client = new Client({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	password: process.env.DB_PW,
-	user: process.env.DB_USER,
-	database: process.env.DB_NAME,
-	ssl: {
-		rejectUnauthorized: false,
-	},
-});
-
-client
-	.connect()
-	.then(() => console.log('connected'))
-	.catch((err) => console.error('connection error', err.stack));
-
-module.exports = client;
-=======
 const { Client } = require('pg');
 require('dotenv').config();
 
@@ -36,4 +14,3 @@ client
 	.catch((err) => console.error('connection error', err.stack));
 
 module.exports = client;
->>>>>>> b293fa9d5ba565d49c75f0bb50ca6eb17e5ce31c
